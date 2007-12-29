4e172890222588a07f32fb0cc861e196df4a9bb9
trieck <trieck@91f4aa99-8ab5-5840-a979-6d3ec3f621a8>
2007-12-29 16:10:08 +0000

    git-svn-id: svn://localhost@17 91f4aa99-8ab5-5840-a979-6d3ec3f621a8

--- lua/first/first.lua
+++ lua/first/first.lua
@@ -1,3 +1,28 @@
 #!/usr/bin/env lua5.1
 
-print("Hello, World!!!")
+-- Euclid's Algorithm
+-- Given two positive integers m and n, find
+-- their greatest common divisor, that is, the largest
+-- positive integer that evenly divides both m and n.
+function euclid(m, n)
+	local r = m % n
+	
+	if r == 0 then return n end	
+	return euclid(n, r)
+end
+
+--print (euclid(800,51))
+
+-- Harmonic series
+function harmonic(n)
+	local t = 0; i = 1
+	
+	while i <= n do
+		t = t + 1/i
+		i = i + 1
+	end
+	
+	return t
+end
+
+print (harmonic(10000000))
