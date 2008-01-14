ad96b528d049cf90fb97eae98ced642d6d114623
trieck <trieck@91f4aa99-8ab5-5840-a979-6d3ec3f621a8>
2008-01-14 01:50:30 +0000

    git-svn-id: svn://localhost@46 91f4aa99-8ab5-5840-a979-6d3ec3f621a8

---
+++ c++/index-0.0.1/src/index.cpp
@@ -0,0 +1,32 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// INDEX.CPP : Hash table index
+//
+// The index consists of hash table containing pointers into the document 
+// vector dat file.  
+//
+// Index blocks are stored as pairs of numbers, with the first representing
+// the offset of the entry in the final table and the second containing
+// the table entry itself.  
+//
+// Copyright (c) 2008 Thomas A. Rieck, All Rights Reserved
+// 
+
+#include "global.h"
+#include "index.h"
+
+#define INDEX_SIZE	(2000000)
+
+/////////////////////////////////////////////////////////////////////////////
+Index::Index() 
+{
+	records = NULL;
+	blocksize = INDEX_SIZE;
+	size = next = 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+Index::~Index()
+{
+    delete [] records;
+}
