2fafd6602588b0917fb2b5c508c27b2c3efceac0
trieck <trieck@91f4aa99-8ab5-5840-a979-6d3ec3f621a8>
2008-02-17 00:18:34 +0000

    git-svn-id: svn://localhost@65 91f4aa99-8ab5-5840-a979-6d3ec3f621a8

---
+++ c++/utility/options.cpp
@@ -0,0 +1,8 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// OPTIONS.CPP : Command line option processing
+//
+// Copyright(c) 2008, Thomas A. Rieck, All Rights Reserved.
+//
+
+#include "options.h"
