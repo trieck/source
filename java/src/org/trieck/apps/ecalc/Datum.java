89f7d57913090ff244445e4788b79c9118fa0a85
trieck <trieck@91f4aa99-8ab5-5840-a979-6d3ec3f621a8>
2008-03-09 19:26:56 +0000

    git-svn-id: svn://localhost@81 91f4aa99-8ab5-5840-a979-6d3ec3f621a8

--- java/src/org/trieck/apps/ecalc/Datum.java
+++
@@ -1,7 +0,0 @@
-package org.trieck.apps.ecalc;
-
-public class Datum {
-
-    double value;   // float value
-    Symbol symbol;  // symbol
-}
--- java/src/org/trieck/pixievm/assembler/ParserException.java
+++
@@ -1,8 +0,0 @@
-package org.trieck.pixievm.assembler;
-
-public class ParserException extends Throwable {
-
-    public ParserException(String ex) {
-        super(ex);
-    }
-}
