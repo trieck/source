ef22803e54b185e2d5cf321f4ff3fe7cb1164821
trieck <trieck@91f4aa99-8ab5-5840-a979-6d3ec3f621a8>
2009-02-06 14:22:09 +0000

    git-svn-id: svn://localhost@138 91f4aa99-8ab5-5840-a979-6d3ec3f621a8

---
+++ c++/WinADF/m68000.cpp
@@ -0,0 +1,11 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// M68000.CPP : M68000 instruction set
+// Copyright(c) 2009 Thomas A. Rieck
+// All Rights Reserved
+//
+
+#include "stdafx.h"
+#include "M68000.h"
+
+
