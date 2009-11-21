597d3985c0337237e7a43bba7e43b988eabbbab7
trieck <trieck@91f4aa99-8ab5-5840-a979-6d3ec3f621a8>
2009-11-21 19:39:42 +0000

    git-svn-id: svn://localhost@309 91f4aa99-8ab5-5840-a979-6d3ec3f621a8

--- c++/unmac/unmac.cpp
+++ c++/unmac/unmac.cpp
@@ -5,27 +5,52 @@
  * and are NOT followed by '\n'.  In this case, we do
  * NOT want to turn them into Windows \r\n or UNIX \n.
  * We want to remove them entirely from the output stream.
+ * 
  */
 #include <stdio.h>
+#include <string.h>
 
-int peekchar()
+static FILE *fpin = stdin, *fpout = stdout;
+
+void cleanup(void)
 {
-	int c = getchar();
+	if (fpin != NULL) {
+		fclose(fpin);
+		fpin = NULL;
+	}
 
-	ungetc(c, stdin);
-
-	return c;
+	if (fpout != NULL) {
+		fclose(fpout);
+		fpout = NULL;
+	}
 }
 
 int main(int argc, char *argv[])
 {
-	int c;
-	while ((c = getchar()) != EOF) {
-		if (c == '\r' && peekchar() != '\n') {
-			;
-		} else {
-			putchar(c);
+	atexit(cleanup);
+
+	for (int i = 1; i < argc; i++) {
+		if (strncmp(argv[i], "--input=", 8) == 0) {
+			if ((fpin = fopen(argv[i]+8, "r")) == NULL) {
+				fprintf(stderr, "cannot open %s.\n", argv[i]+8);
+				return 1;
+			}
 		}
+
+		if (strncmp(argv[i], "--output=", 9) == 0) {
+			if ((fpout = fopen(argv[i]+9, "w")) == NULL) {
+				fprintf(stderr, "cannot open %s.\n", argv[i]+9);
+				return 1;
+			}
+		}
+	}
+
+	int c, last = EOF;
+	while ((c = getc(fpin)) != EOF) {
+		if (c != '\r') {
+			putc(c, fpout);
+		}
+		last = c;
 	}
 
 	return 0;
