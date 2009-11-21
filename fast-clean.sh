f378ac8597cc8822fab2f16c45987bb1b2bb638c
trieck <trieck@91f4aa99-8ab5-5840-a979-6d3ec3f621a8>
2009-11-21 16:13:33 +0000

    git-svn-id: svn://localhost@304 91f4aa99-8ab5-5840-a979-6d3ec3f621a8

--- fast-clean.sh
+++ fast-clean.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-EXTENS=""
+FILTER=""
 count=0
 
 exec < ./remove-extensions
 while read line
 do
 	if [ $count -gt 0 ]; then
-		EXTENS="$EXTENS|";
+		FILTER="$FILTER -o ";
 	fi
-	EXTENS="$EXTENS$line";	
+	FILTER="$FILTER -name *.$line";	
 	count=$[$count+1];
 done
 
-echo cleaning intermediate files...
-find . -regextype posix-egrep -iregex ".+\.($EXTENS)" -exec ./clean-file.sh \{} \;
+find . \( $FILTER \) -print -exec rm -rf \{} \;
+
 
 
 
