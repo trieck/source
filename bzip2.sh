b0024891cb978bdefd8bd45b81ae978070433462
trieck <trieck@91f4aa99-8ab5-5840-a979-6d3ec3f621a8>
2009-11-22 15:24:28 +0000

    git-svn-id: svn://localhost@315 91f4aa99-8ab5-5840-a979-6d3ec3f621a8

--- bzip2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. ./fast-clean.sh
-
-echo Making archive...
-tar --exclude-vcs -cjvf source.tar.bz2 *
-
---
+++ make-archive.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#. ./fast-clean.sh
+
+echo Making archive...
+#tar --exclude-vcs -cjvf source.tar.bz2 *
+
