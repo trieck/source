792adb79a9c8c977e51975436fc8b1b1799d6a99
trieck <trieck@91f4aa99-8ab5-5840-a979-6d3ec3f621a8>
2009-11-23 18:36:36 +0000

    git-svn-id: svn://localhost@346 91f4aa99-8ab5-5840-a979-6d3ec3f621a8

--- c++/libcdio/libcdio_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo ----------Building project:[ iso_read - Debug ]----------
-	@cd "iso_read" && "D:\MinGW\bin\mingw32-make.exe"  -j 1 -f "iso_read.mk"
-clean:
-	@echo ----------Cleaning project:[ iso_read - Debug ]----------
-	@cd "iso_read" && "D:/MinGW/bin/mingw32-make.exe"  -j 1 -f "iso_read.mk" clean
