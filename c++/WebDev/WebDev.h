60ac5cb2954572f7f6b9f74955fc543de1021992
trieck <trieck@91f4aa99-8ab5-5840-a979-6d3ec3f621a8>
2010-08-21 07:22:18 +0000

    git-svn-id: svn://localhost@506 91f4aa99-8ab5-5840-a979-6d3ec3f621a8

---
+++ c++/WebDev/WebDev.h
@@ -0,0 +1,33 @@
+
+// WebDev.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CWebDevApp:
+// See WebDev.cpp for the implementation of this class
+//
+
+class CWebDevApp : public CWinApp
+{
+public:
+	CWebDevApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CWebDevApp theApp;
+
