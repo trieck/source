a54c4e6de1d55fa3f8249fff720efa5d84601d6d
trieck <trieck@91f4aa99-8ab5-5840-a979-6d3ec3f621a8>
2010-08-29 00:14:22 +0000

    git-svn-id: svn://localhost@510 91f4aa99-8ab5-5840-a979-6d3ec3f621a8

---
+++ asm/x64/first.asm
@@ -0,0 +1,26 @@
+; first x64 program
+;
+bits 64
+
+global main
+
+extern MessageBoxA
+extern ExitProcess
+
+SECTION .data
+title 	db 'The 64-bit world of Windows & assembler...', 0
+message	db 'Hello World!', 0
+
+SECTION .text
+main:
+	sub rsp, 28h 		; shadow space, aligns stack
+
+	xor r9d, r9d		; uType = MB_OK
+	mov r8, title		; LPCSTR lpCaption
+	mov rdx, message	; LPCSTR lpText
+	xor rcx, rcx		; hWnd = HWND_DESKTOP
+	call MessageBoxA
+	mov ecx, eax		; uExitCode = MessageBox(...)
+	call ExitProcess
+	
+	ret
