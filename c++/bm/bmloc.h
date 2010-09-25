185e96ff6e4fdd49bdca9c3df0c0fa31b36388a0
trieck <trieck@91f4aa99-8ab5-5840-a979-6d3ec3f621a8>
2010-09-25 01:07:55 +0000

    git-svn-id: svn://localhost@601 91f4aa99-8ab5-5840-a979-6d3ec3f621a8

--- c++/bm/bmloc.h
+++ c++/bm/bmloc.h
@@ -16,9 +16,9 @@ int AssertValidBitmap(LPSTR szFileName);
 int AssertDevCapable(HWND hWnd, INT nFlags);
 HBITMAP CreateBitmapFromFile(HDC hDC, LPSTR szFileName);
 BOOL CALLBACK DlgProc(	HWND  hwndDlg,	// handle to dialog box
-                       UINT  uMsg,		// message
-                       WPARAM  wParam,	// first message parameter
-                       LPARAM  lParam);
+        UINT  uMsg,		// message
+        WPARAM  wParam,	// first message parameter
+        LPARAM  lParam);
 int ExtractBMInfo(LPSTR szFileName, LPBITMAPFILEHEADER pBMFH, LPBITMAPINFO* ppBMI, LPVOID* ppBits);
 void FillDlgInfo(HWND hDlg, LPSTR szFileName, BITMAPFILEHEADER bmfh, LPBITMAPINFO pBMI);
 LONG ScanAlign (LONG pWidth);
