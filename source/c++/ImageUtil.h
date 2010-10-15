cf2d52f481930a01f68bb9b4ca2742344eb8abf7
trieck <trieck@91f4aa99-8ab5-5840-a979-6d3ec3f621a8>
2010-10-15 22:06:56 +0000

    git-svn-id: svn://localhost@624 91f4aa99-8ab5-5840-a979-6d3ec3f621a8

--- source/c++/ImageUtil.h
+++
@@ -1,8 +0,0 @@
-
-#pragma once
-
-BYTE PixelDigit(const CImage &image, const CPoint &pt);
-BYTE PixelDigit(const CImage &image, int x, int y);
-bool IsValidPoint(const CImage &image, const CPoint &pt);
-bool IsValidPoint(const CImage &image, int x, int y);
-CPoint Neighbor(int x, int y, int k);
