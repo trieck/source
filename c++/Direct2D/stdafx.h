cb1c7c94ca3a79d50f6a6db687b007075c340ef7
Thomas A. Rieck <trieck@gmail.com>
2019-12-09 14:45:14 -0500

    Upgrade

--- c++/Direct2D/stdafx.h
+++ c++/Direct2D/stdafx.h
@@ -1,11 +1,5 @@
 #pragma once
 
-// Change these values to use different versions
-#define WINVER		0x0500
-#define _WIN32_WINNT	0x0501
-#define _WIN32_IE	0x0501
-#define _RICHEDIT_VER	0x0200
-
 #include <atlbase.h>
 #include <atltypes.h>
 #include <atlapp.h>
