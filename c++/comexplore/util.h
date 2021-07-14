6c55afdd8c8a55fa991c64ad161e1f46082a4b07
Thomas A. Rieck <tom.rieck@maxar.com>
2021-07-14 10:51:53 -0400

    remove COM Explorer, as it's in its own repo.

--- c++/comexplore/util.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-/////////////////////////////////////////////////////////////////////////////
-CString Escape(LPCTSTR lpszInput);
-CString GetScodeString(SCODE sc);
-CString TYPEDESCtoString(LPTYPEINFO pTypeInfo, TYPEDESC* pTypeDesc);
-CString VTtoString(VARTYPE vt);
